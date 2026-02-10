7bf10cd10b90e091d2ddd2ea1691758474ffb838
mohameddsalah60 <mohamedsalahdev2004@gmail.com>
2026-02-10 12:09:39 +0200

    chore: screen util Init package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sadaqa_app/core/utils/app_colors.dart';
 
 void main() {
@@ -10,10 +11,17 @@ class SadaqaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(scaffoldBackgroundColor: AppColors.scaffoldColor),
-      home: Scaffold(body: Text("data")),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(scaffoldBackgroundColor: AppColors.scaffoldColor),
+          home: Scaffold(body: Text("data")),
+        );
+      },
     );
   }
 }
