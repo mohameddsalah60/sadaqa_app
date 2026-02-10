d93b9c0c595511cf0221a4e91d973c7e8ff98c90
mohameddsalah60 <mohamedsalahdev2004@gmail.com>
2026-02-10 12:03:29 +0200

    utlis: create app colors class

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const scaffoldColor = Color(0xff5b3454);
+  static const whiteColor = Color(0xffffffff);
+  static const blackColor = Color(0xff000000);
+  static const primaryColor = Color(0xff5b3454);
+}
